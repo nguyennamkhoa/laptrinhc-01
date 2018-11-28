f29e2b0bbbc214c9405005ee776da3809b684ba7
Nam Khoa <nguyennamkhoalt@gmail.com>
2018-11-28 20:02:50 +0700

    thumuc

---
+++ New folder/buoi3.1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+main(){
+int a;
+cout<<"Nhap so thang ";
+cin>>a;
+if(a <= 1 && a<= 12){
+	}
+	if(a>=1&&a<=3){
+		cout<<"quy 1";
+	}
+	if(a>=4&&a<=6){
+		cout<<"quy 2";
+	}
+	if(a>=7&&a<=9){
+		cout<<"quy 3";
+		
+	}
+	if(a>=10&&a<=12){
+		cout<<"quy 4";
+		
+	
+	}
+		
+
+		
+}
